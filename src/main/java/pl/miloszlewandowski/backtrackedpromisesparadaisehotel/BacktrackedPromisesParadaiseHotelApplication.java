be1f88a08a7f8806eb92b4004e4cbfd9299199a3
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-27 23:45:28 +0200

    Initial commit

---
+++ src/main/java/pl/miloszlewandowski/backtrackedpromisesparadaisehotel/BacktrackedPromisesParadaiseHotelApplication.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski.backtrackedpromisesparadaisehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacktrackedPromisesParadaiseHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacktrackedPromisesParadaiseHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/miloszlewandowski/backtrackedpromisesparadaisehotel/BacktrackedPromisesParadaiseHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski.backtrackedpromisesparadaisehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacktrackedPromisesParadaiseHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
