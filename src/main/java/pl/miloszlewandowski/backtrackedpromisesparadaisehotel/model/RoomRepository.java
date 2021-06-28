b116766934da06a48ad1354ed235cb3e02f4e6da
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-28 23:57:47 +0200

    change Guest to Room

--- src/main/java/pl/miloszlewandowski/backtrackedpromisesparadaisehotel/model/RoomRepository.java
+++ src/main/java/pl/miloszlewandowski/backtrackedpromisesparadaisehotel/model/RoomRepository.java
@@ -2,6 +2,6 @@ package pl.miloszlewandowski.backtrackedpromisesparadaisehotel.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends JpaRepository<Guest, Integer> {
+public interface RoomRepository extends JpaRepository<Room, Integer> {
 
 }
