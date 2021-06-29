86d4dc4b9a9e0e57c2747285dae2598c118750f6
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-06-29 04:51:24 +0200

    Delete outdated Room JPA Repository

--- src/main/java/pl/miloszlewandowski/backtrackedpromisesparadaisehotel/model/RoomRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.miloszlewandowski.backtrackedpromisesparadaisehotel.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Integer> {
-
-}
